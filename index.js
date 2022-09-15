74f6fbee2cf8a81abb31bb96a2623d5e3346b361
SharukhGV <sharukhvelupillai@pursuit.org>
2022-09-15 18:33:07 -0400

    fixed coordinates,google map search address functionality

--- index.js
+++ index.js
@@ -1,139 +1,184 @@
+//DECLARING THE GOOGLE MAPS MAP VARIABLE
+var map;
+//**********************************************************************************************************
+//THE LINE OF CODE BELOW WILL SELECT THE DIV THAT HAS AN ID OF MAP IN THE HTML DOCUMENT AND RENDER THE GOOGLE MAPS UPON PAGELOAD
+let googleMAPS = document.getElementById("map");
+
+//**********************************************************************************************************
+//DARK SKY RESERVE COORDINATES THAT WILL BE RENDERED ON MAP UPON PAGE LOAD
+//**********************************************************************************************************
+var cherry = { lat: 41.66322605092642, lng: -77.82340454232315 };
+var sandDunes = { lat: 37.7940008252381, lng: -105.59327612114384 };
+var bigbend = { lat: 29.281262599614543, lng: -103.25034877571737 };
+ //---------------------------------------->THIS ONE
+//**********************************************************************************************************
 
 
-
-let coordinates = [{ lat: 41.66322605092642, lng: -77.82340454232315 }]
-let searches = ["47-10%20Austell%20Pl%202nd%20floor%2C%20Long%20Island%20City%2C%20NY%2011101"]
-var map; // google maps api
-
-console.log(coordinates)
+//FROM THE SEARCH BAR, WHEN A USER CLICKS 
 function geocodeME(event){
-    event.preventDefault()
-let x = document.querySelector("#site-search").value
-searches.push(x.replace(" ", "%20"))
-}
 
+  event.preventDefault()
 
-//GEOCODING API
-let geoapiid = "pk.3b76d3390feef27f7d252b1dc6788c2c"
+  // setTimeout(() => {
+    //THE CODE BELOW WILL TAKE THE VALUE THAT IS IN THE TEXT FIELD IN THE SEARCH BAR AND SAVE IT TO VARIABLE X
+    var x = document.getElementById("site-search").value;
+    // THE CODE BELOW WILL CHANGE THE STRING VALUE FROM THE SEARCH AND CONVERT IT TO A STIRING THAT CAN BE READ BY THE LOCATION IQ API. IT REPLACES THE SPACES WITH %20 AND COMMAS WITH %2C. THIS CODE WILL BE INSERTED INTO CITYMAIN AS PART OF THE COMPLETE URL FOR FETCHING THE API
+    var y = x.replaceAll(" ", "%20").replaceAll(",","%2C")
+//THE CODE BELOW IS THE API KEY
+  let geoapiid = "pk.3b76d3390feef27f7d252b1dc6788c2c"
+  //THE CODE BELOW WILL TAKE THE FIRT PART OF THE API URL WHICH INCLUDESS THE API KEY INSERTED VIA OBJECT LITERAL
   let city1 = `https://us1.locationiq.com/v1/search?key=${geoapiid}&q=`
-  
-  let city2 = searches[searches.length-1]
+    //THE CODE BELOW WILL TAKE THE LAST PART OF THE API URL WHICH INCLUDES THE API KEY
   let city3 = "&format=json"
-  let cityMAIN = `${city1}${city2}${city3}`
+  //THE CODE BELOW WILL COMBINE THE THREE STRINGS TO MAKE A COMPLETE URL FOR THE FETCH API FOR LOCATION-IQ API
+  let cityMAIN = `${city1}${y}${city3}`
+//THE CODE BELOW WILL GET THE DATA FROM THE URL WHICH BASICALLY IS THE STREET ADDRESS CONVERTED INTO COORDINATES FOR A MAP (LATITUDE AND LONGITUDE)
+  fetch(`${cityMAIN}`)
+  //THE CODE BELOW WILL TAKE THE JSON DATA AND CONVERT IT TO JAVASCRIPT FOR EASY ACCESS THEN TAKE THAT SAME DATA TO BE IMPLEMENTED IN VARIOUS WAYS
+  .then((response) => response.json()).then((data) => {
+  //THE DATA FETCHED WILL RENDER FORWARD GEOCODED ADDRESS , WHICH MEANS IT WILL TAKE THE STREET INPUTTED FROM THE SEARCH BAR AND CONVERT IT TO LATITUDE AND LONGITUDE COORDINATES
+  var latitude = Number(data[0].lat);
+  var longitude = Number(data[0].lon);
 
-console.log(cityMAIN)
-console.log("___________________________________")
+//THE CODE BELOW TAKES THE RENDERED LATITUDE & LONGITUDE AND SAVES IT TO A VARIABLE
+let homelocal = { lat: latitude, lng: longitude }
+//THE CODE BELOW MAKES A NEW MARKER EVERY TIME THE BUTTON IS CLICKED WITH A VALID ADDRESS INPUTTED
+    let marker0 = new google.maps.Marker({
+      position: homelocal,
+      icon: {                             
+        url: "telescopeicon.png"
+      }
+    })
+    marker0.setMap(map);
 
-
-fetch(cityMAIN)
-.then((response) => response.json()).then((data) => {
-
-let latitude = data[0].boundingbox[0]
-let longitude = data[0].boundingbox[2]
-
-coordinates.push({lat: latitude, lng: longitude})
+    event.preventDefault()
 
 }).catch((error) => {
-    
-    console.log(error)
-  }) //END OF FETCH FROM GEOCODEME***************************
+      
+      console.log(error)
+    }) 
+  }
 
+  
+//**********************************************************************************************************
+//**********************************************************************************************************
+//**********************************************************************************************************
+// GOOGLE MAPS - THE CODE BELOW CREATES THE MARKERS FOR THE DARK SKY PRESERVES
+//**********************************************************************************************************
+//**********************************************************************************************************
+//**********************************************************************************************************
 
-
-
-function initMap() {//GOOGLE MAPS API
-
-//MAPS API WITH MARKERS
-// Initialize and add the map
-
-
-
-
-const homelocal = coordinates[coordinates.length-1]
-    // The map, centered at Uluru
-    
-    const marker0 = new google.maps.Marker({
-        position: homelocal,
-        map: map,
-      });
-    // The location of big bend
-    const bigbend = { lat: 29.281262599614543, lng: -103.25034877571737 };
-   
-    // The marker, positioned at Big Bend National Park
+//Google MAp initialized at startup that will contain markers of the Dark Sky preserves
+function initMap() {
+map = new google.maps.Map(googleMAPS, {
+  zoom: 4,
+  center: cherry})
 
     const marker = new google.maps.Marker({
       position: bigbend,
       map: map,
     });
 
-    const cherry = { lat: 41.66322605092642, lng: -77.82340454232315 };
-
-    // The marker, positioned at Cherry Springs State Park
-
     const marker2 = new google.maps.Marker({
       position: cherry,
       map: map,
     });
 
-    const sandDunes = { lat: 37.7940008252381, lng: -105.59327612114384 };
-
-    // The marker, positioned at Great Sand Dunes National Park and Preserve
     const marker3 = new google.maps.Marker({
       position: sandDunes,
       map: map,
     });
 
-if(coordinates.length>2){ map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: homelocal,
-  })}
-else{
-     map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: cherry,})
-  }
-  
 }
+//**********************************************************************************************************
+//**********************************************************************************************************
+//**********************************************************************************************************
+// GOOGLE MAPS - THE CODE ABOVE CREATES THE MARKERS FOR THE DARK SKY PRESERVES
+//**********************************************************************************************************
+//**********************************************************************************************************
+//**********************************************************************************************************
+//**********************************************************************************************************
+//**********************************************************************************************************
+//******************************************************************************************************* */
 
+//**********************************************************************************************************
+//********************************************************************************************************** */
+
+
+
+
+//THE CODE BELOW IS THE EVENT LISTENSER THAT WILL RUN THE LOCATION IQ API AND INTEGRATE IT WITH THE GOOGLE API ABOVE( WHEN IT IS CLICKED):
+//THIS CODE WILL SELECT THE BUTTON IN THE HTML DOCUMENT
+  let button = document.querySelector("#button");
+  // THIS CODE WILL "LISTEN" FOR THE BUTTON CLICK TO RUN THE FUNCTION "GEOCODEME"
+  button.addEventListener("click",geocodeME);
+
+
+
+// THE CODE BELOW WILL INITIALIZE THE INITMAP INITIAL GOOGLE MARKER AND MAP CREATION FUNCTION
   window.initMap = initMap;
 
 
-  console.log("https://visible-planets-api.herokuapp.com/v2?latitude=32&longitude=-98")
-  //VISIBLE PLANETS API
-//https://github.com/csymlstd/visible-planets-api  --> github link
-  fetch("https://visible-planets-api.herokuapp.com/v2?latitude=32&longitude=-98")
-  .then((response) => response.json()).then((data) => {
+      //WILL WORK ON OTHER APIS LATER TIME BELOW
 
-    console.log(data)
+//**********************************************************************************************************
+//******************************************************************************************************* */
+//**********************************************************************************************************
+//********************************************************************************************************** */
+//**********************************************************************************************************
+//******************************************************************************************************* */
+//**********************************************************************************************************
+//********************************************************************************************************** */
+//**********************************************************************************************************
+//********************************************************************************************************** */
+// //**********************************************************************************************************
+//PLANT APIS
+//   fetch("https://visible-planets-api.herokuapp.com/v2?latitude=32&longitude=-98")
+//   .then((response) => response.json()).then((data) => {
+
+//     // console.log(data)
 
 
-}).catch((error) => {
+// }).catch((error) => {
     
-    console.log(error)
-  }) 
-
-
-// //WEATHER API
-let apiid = "a6e46b7a3f90a5e45660927441f74edf"
-let latty = (coordinates[coordinates.length-1]).lat
-let longy = (coordinates[coordinates.length-1]).lng
+//     console.log(error)
+//   }) 
+//**********************************************************************************************************
+//WEATHER AND MOON API
+// let apiid = "a6e46b7a3f90a5e45660927441f74edf"
+// // let latty = (coordinates[coordinates.length-1]).lat
+// // let longy = (coordinates[coordinates.length-1]).lng
  
-let appidOPENWEATHER = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiid}`
-console.log(appidOPENWEATHER)
+// let appidOPENWEATHER = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiid}`
+// console.log(appidOPENWEATHER)
 
-// fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latty}&lon=${longy}&exclude={part}&appid={${apiid}}`)
+// // fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latty}&lon=${longy}&exclude={part}&appid={${apiid}}`)
 
-  fetch(appidOPENWEATHER)
-  .then((response) => response.json()).then((data) => {
+//   fetch(appidOPENWEATHER)
+//   .then((response) => response.json()).then((data) => {
 
-}).catch((error) => {
+// }).catch((error) => {
     
-    console.log(error)
-  })
+//     console.log(error)
+//   })
+//**********************************************************************************************************
 
 
+// console.log(coordinates);
+// console.log(homelocal)
+// console.log(searches)
 
 
-  let form = document.querySelector("#form");
+//       var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+// var mapOptions = {
+//   zoom: 4,
+//   center: myLatlng
+// }
+// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  form.addEventListener("submit",geocodeME)
+// var marker = new google.maps.Marker({
+//     position: myLatlng,
+//     title:"Hello World!"
+// });
+
+  window.initMap = initMap;
